5a52f6cdc1d15d2ca79bb509dd718b8be86b7f0f
Kris <emineo@abv.bg>
2025-01-25 20:21:59 +0200

    refactor register

--- auth-service/internal/services/auth_service.go
+++ auth-service/internal/services/auth_service.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"wall-e-go/auth-service/internal/models"
 	"wall-e-go/auth-service/internal/repository"
-	jwt_util "wall-e-go/auth-service/internal/util"
+	jwt "wall-e-go/auth-service/internal/util"
 	errors "wall-e-go/common"
 
 	"golang.org/x/crypto/bcrypt"
@@ -20,26 +20,49 @@ func NewAuthService(userRepo *repository.UserRepository) *AuthService {
 
 func (authService AuthService) RegisterUser(newUser models.User) (string, error) {
 
-	existingUser, err := authService.UserRepository.GetUserByUsername(newUser.Username)
-	if err != nil && err != sql.ErrNoRows {
-		return "", errors.WrapError(errors.ErrInternal, "Error checking user existence")
-	}
-	if existingUser != nil {
-		return "", errors.ErrAlreadyExists
+	if err := authService.handleExistingUser(newUser.Username); err != nil {
+		return "", err
 	}
 
+	if err := authService.createUser(&newUser); err != nil {
+		return "", err
+	}
+
+	token, err := authService.generateToken(newUser.Username)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (authService AuthService) handleExistingUser(username string) error {
+	existingUser, err := authService.UserRepository.GetUserByUsername(username)
+	if err != nil && err != sql.ErrNoRows {
+		return errors.WrapError(errors.ErrInternal, "Error checking user existence")
+	}
+	if existingUser != nil {
+		return errors.ErrAlreadyExists
+	}
+	return nil
+}
+
+func (authService AuthService) createUser(newUser *models.User) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", errors.WrapError(errors.ErrInternal, "Error hashing password")
+		return errors.WrapError(errors.ErrInternal, "Error hashing password")
 	}
 	newUser.Password = string(hashedPassword)
 
-	if err := authService.UserRepository.CreateUser(newUser); err != nil {
-		return "", errors.WrapError(errors.ErrInternal, "Error creating user")
+	if err := authService.UserRepository.CreateUser(*newUser); err != nil {
+		return errors.WrapError(errors.ErrInternal, "Error creating user")
 	}
+	return nil
+}
 
-	jwtUtil := jwt_util.JWTUtil{}
-	token, err := jwtUtil.GenerateToken(newUser.Username)
+func (authService AuthService) generateToken(username string) (string, error) {
+	jwtUtil := jwt.JWTUtil{}
+	token, err := jwtUtil.GenerateToken(username)
 	if err != nil {
 		return "", errors.WrapError(errors.ErrInternal, "Error generating token")
 	}
--- auth-service/internal/util/jwt_util.go
+++ auth-service/internal/util/jwt_util.go
@@ -1,4 +1,4 @@
-package jwt_util
+package jwt
 
 import (
 	"os"
