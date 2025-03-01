a798fe1248ab395b317e853609efdace7cb67305
Kris <emineo@abv.bg>
2025-03-01 11:36:42 +0200

    add interface for wallet & extract token func

--- wallet-service/internal/service/wallet.go
+++ wallet-service/internal/service/wallet.go
@@ -13,40 +13,29 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type WalletService struct {
+type WalletService interface {
+	CreateWallet(ctx context.Context, req *walletpb.CreateWalletRequest) (*walletpb.CreateWalletResponse, error)
+}
+
+type WalletServiceImpl struct {
 	walletpb.UnimplementedWalletServiceServer
 	walletRepo data.WalletRepository
 	jwtUtil    jwt.JWTUtil
 }
 
-func NewWalletService(walletRepo data.WalletRepository, jwtUtil jwt.JWTUtil) *WalletService {
-	return &WalletService{
+func NewWalletService(walletRepo data.WalletRepository, jwtUtil jwt.JWTUtil) *WalletServiceImpl {
+	return &WalletServiceImpl{
 		walletRepo: walletRepo,
 		jwtUtil:    jwtUtil,
 	}
 }
 
-func (s *WalletService) CreateWallet(ctx context.Context, req *walletpb.CreateWalletRequest) (*walletpb.CreateWalletResponse, error) {
+func (s *WalletServiceImpl) CreateWallet(ctx context.Context, req *walletpb.CreateWalletRequest) (*walletpb.CreateWalletResponse, error) {
 	var newWallet data.Wallet
-	// Extract token from gRPC metadata
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok || len(md["authorization"]) == 0 {
-		return nil, status.Errorf(codes.Unauthenticated, "authorization header is missing")
-	}
 
-	authHeader := md["authorization"][0]
-	token := authHeader
-	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
-		token = authHeader[7:]
-	}
-
-	// Validate token and get user_id
-	userID, err := s.jwtUtil.ValidateToken(token)
+	userID, err := s.extractUserIdAndValidateToken(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "invalid token : %v", err)
-	}
-	if userID == 0 {
-		return nil, status.Errorf(codes.Internal, "error processing token")
+		return nil, err
 	}
 
 	// Check if wallet with such name already exists for this user
@@ -67,4 +56,30 @@ func (s *WalletService) CreateWallet(ctx context.Context, req *walletpb.CreateWa
 	}
 
 	return &walletpb.CreateWalletResponse{WalletId: strconv.Itoa(walletID)}, nil
+}
+
+func (s *WalletServiceImpl) extractUserIdAndValidateToken(ctx context.Context) (int, error) {
+
+	// Extract token from gRPC metadata
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok || len(md["authorization"]) == 0 {
+		return 0, status.Errorf(codes.Unauthenticated, "authorization header is missing")
+	}
+
+	authHeader := md["authorization"][0]
+	token := authHeader
+	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
+		token = authHeader[7:]
+	}
+
+	// Validate token and get user_id
+	userID, err := s.jwtUtil.ValidateToken(token)
+	if err != nil {
+		return 0, status.Errorf(codes.Unauthenticated, "invalid token : %v", err)
+	}
+	if userID == 0 {
+		return 0, status.Errorf(codes.Internal, "error processing token")
+	}
+
+	return userID, nil
 }
