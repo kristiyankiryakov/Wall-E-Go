5e03c68cfdc073544f1c8ccd55b49020be6ed27b
Kris <emineo@abv.bg>
2025-03-07 13:15:11 +0200

    refactor transaction & wallet schema

--- transaction-service/migrations/000001_create_table_transactions.up.sql
+++ transaction-service/migrations/000001_create_table_transactions.up.sql
@@ -3,13 +3,15 @@ BEGIN;
 CREATE TYPE transaction_type AS ENUM('DEPOSIT', 'WITHDRAW');
 
 CREATE TABLE IF NOT EXISTS transactions(
-id SERIAL PRIMARY KEY,
-wallet_id INTEGER NOT NULL,
+id uuid DEFAULT gen_random_uuid(),
+wallet_id uuid NOT NULL,
 amount DECIMAL(15,2) NOT NULL,
 type TRANSACTION_TYPE NOT NULL,
 idempotency_key VARCHAR(255) UNIQUE,
 status VARCHAR(20) DEFAULT 'PENDING',
-created_at TIMESTAMP DEFAULT NOW()
+created_at TIMESTAMP DEFAULT NOW(),
+
+PRIMARY KEY(id)
 );
 
 COMMIT;
--- wallet-service/migrations/000001_create_wallet_table.up.sql
+++ wallet-service/migrations/000001_create_wallet_table.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS wallets(
-id SERIAL PRIMARY KEY,
+id uuid DEFAULT gen_random_uuid(),
 user_id INT NOT NULL,
 name VARCHAR(255) NOT NULL,
-balance decimal(15,2) DEFAULT 0.00,
+balance decimal(15,2) DEFAULT 0.00 CHECK (balance >= 0),
 created_at TIMESTAMP DEFAULT NOW(),
-updated_at TIMESTAMP DEFAULT NOW()
+updated_at TIMESTAMP DEFAULT NOW(),
+
+PRIMARY KEY(id)
 );
